6231d9347d94b182da9a24ba823e421f1ba97d91
VolodymyrV <kotsiubkov@gmail.com>
2025-09-22 17:34:44 +0300

    midle

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import axios from 'axios';
-
 import './css/comon.css';
 
-const API_KEY = "43173775-fc7269b10cca3a5d436001063";
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const API_KEY = "52413922-df6c611514e1fbd211f3ba691";
 
 const form = document.querySelector(".form");
 const gallery = document.querySelector(".gallery")
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 
 form.addEventListener("submit", handlerSubmit);
@@ -17,7 +31,13 @@ function handlerSubmit(event) {
 
 
     if(searchResulr === "") {
-        alert("You are valyanok")
+        iziToast.error(
+        {
+          message: "Sorry, you didn’t enter any value!",
+          position: 'topRight',
+          backgroundColor: '#EF4040',
+          messageColor: '#ffffff',
+        })
         return;
     }
     
@@ -31,12 +51,24 @@ function handlerSubmit(event) {
         }
     })
         .then((res) => {
+            if(res.data.hits.length === 0) {
+                iziToast.error(
+                {
+                message: "Sorry, there are no images matching your search query. Please try again!",
+                position: 'topRight',
+                backgroundColor: '#EF4040',
+                messageColor: '#ffffff',
+                })
+            }
+                       
             console.log(res);
-            gallery.insertAdjacentHTML("beforeend", createMarkUp(res.data.hits))
+            gallery.insertAdjacentHTML("beforeend", createMarkUp(res.data.hits));
+            lightbox.refresh();
             
         })
         .catch((error) => {
             console.log(error);
+            
             
         })
 
@@ -49,15 +81,17 @@ function handlerSubmit(event) {
 
 
 function createMarkUp(arr) {
-    return arr.map(({ previewURL, likes, views, comments, downloads }) => `
+    return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
         <li class="photo-card">
-            <img src="${previewURL}" height="" weight=""/>
-             <div class="photo-info-labels">
+            <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" height="200" width="360" class="img-card"/>
+             </a>
+            <div class="photo-info-labels">
                 <span>Likes</span>
                 <span>Views</span>
                 <span>Comments</span>
                 <span>Downloads</span>
-            </div>
+                </div>
 
             <div class="photo-info-values">
                 <span>${likes}</span>
@@ -69,3 +103,7 @@ function createMarkUp(arr) {
         </li>
         `).join("")
 }
+
+
+
+    
