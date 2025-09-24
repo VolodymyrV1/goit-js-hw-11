d9e74b49feb60dd0858c1beab83e59b7e59a4786
VolodymyrV <kotsiubkov@gmail.com>
2025-09-24 17:00:54 +0300

    fin

--- src/main.js
+++ src/main.js
@@ -1,109 +1,60 @@
-import axios from 'axios';
 import './css/comon.css';
 
-// Описаний у документації
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+import getImagesByQuery from "./js/pixabay-api";
+import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions";
 
-
-const API_KEY = "52413922-df6c611514e1fbd211f3ba691";
 
 const form = document.querySelector(".form");
-const gallery = document.querySelector(".gallery")
-
-let lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250,
-});
 
 
 form.addEventListener("submit", handlerSubmit);
 
 function handlerSubmit(event) {
     event.preventDefault();
-    gallery.innerHTML = "";
-    const searchResulr = event.target.elements['search-text'].value.trim();
+    clearGallery();
+    const searchResult = event.target.elements['search-text'].value.trim();
 
-
-    if(searchResulr === "") {
+    if(searchResult === "") {
         iziToast.error(
         {
           message: "Sorry, you didn’t enter any value!",
           position: 'topRight',
           backgroundColor: '#EF4040',
           messageColor: '#ffffff',
-        })
+            })
+        form.reset();
         return;
     }
+
+    showLoader();
     
-    axios("https://pixabay.com/api/", {
-        params: {
-            key: API_KEY,
-            q: searchResulr,
-            image_type: "photo",
-            orientation: "horizontal",
-            safesearch: true
-        }
-    })
-        .then((res) => {
-            if(res.data.hits.length === 0) {
+    getImagesByQuery(searchResult)
+        .then((data) => {
+           
+            if(data.hits.length === 0) {
                 iziToast.error(
                 {
                 message: "Sorry, there are no images matching your search query. Please try again!",
                 position: 'topRight',
                 backgroundColor: '#EF4040',
                 messageColor: '#ffffff',
-                })
-            }
-                       
-            console.log(res);
-            gallery.insertAdjacentHTML("beforeend", createMarkUp(res.data.hits));
-            lightbox.refresh();
-            
+                    })
+                return;
+            }       
+            createGallery(data.hits);
         })
         .catch((error) => {
-            console.log(error);
-            
-            
+            console.log(error);     
         })
-
-
-        form.reset()
+        .finally(() => {            
+            hideLoader();
+        })
     
-
-    
+        form.reset()   
 }
-
-
-function createMarkUp(arr) {
-    return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-        <li class="photo-card">
-            <a href="${largeImageURL}">
-            <img src="${webformatURL}" alt="${tags}" height="200" width="360" class="img-card"/>
-             </a>
-            <div class="photo-info-labels">
-                <span>Likes</span>
-                <span>Views</span>
-                <span>Comments</span>
-                <span>Downloads</span>
-                </div>
-
-            <div class="photo-info-values">
-                <span>${likes}</span>
-                <span>${views}</span>
-                <span>${comments}</span>
-                <span>${downloads}</span>
-            </div>
-            
-        </li>
-        `).join("")
-}
-
 
 
     
