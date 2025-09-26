b36b1deb52b8693ec5c9c0f414cad7cae092ae54
VolodymyrV <kotsiubkov@gmail.com>
2025-09-26 15:20:32 +0300

    fin2

--- src/main.js
+++ src/main.js
@@ -17,14 +17,8 @@ function handlerSubmit(event) {
     clearGallery();
     const searchResult = event.target.elements['search-text'].value.trim();
 
-    if(searchResult === "") {
-        iziToast.error(
-        {
-          message: "Sorry, you didn’t enter any value!",
-          position: 'topRight',
-          backgroundColor: '#EF4040',
-          messageColor: '#ffffff',
-            })
+    if (searchResult === "") {
+        showToast("Sorry, you didn’t enter any value!");
         form.reset();
         return;
     }
@@ -34,19 +28,14 @@ function handlerSubmit(event) {
     getImagesByQuery(searchResult)
         .then((data) => {
            
-            if(data.hits.length === 0) {
-                iziToast.error(
-                {
-                message: "Sorry, there are no images matching your search query. Please try again!",
-                position: 'topRight',
-                backgroundColor: '#EF4040',
-                messageColor: '#ffffff',
-                    })
+            if (data.hits.length === 0) {
+                showToast("Sorry, there are no images matching your search query. Please try again!");
                 return;
             }       
             createGallery(data.hits);
         })
         .catch((error) => {
+            showToast(`EROR, ${error}`);
             console.log(error);     
         })
         .finally(() => {            
@@ -56,5 +45,12 @@ function handlerSubmit(event) {
         form.reset()   
 }
 
-
+function showToast(message, ) {
+    iziToast.error({
+        message,
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        messageColor: '#ffffff',
+    });
+}
     
